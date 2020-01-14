aebe25ddb6c0f7a3a0dda15d4e1a1046680dfb03
sijinyu <sijinyudev@gmail.com>
2020-01-14 22:59:55 +0900

    Todo - commit - 01-14

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
+import TodoListTemplate from './Components/TodoListTemplate';
+import Form from './Components/Form';
+import TodoItemList from './Components/TodoItemList';
+
 
 class App extends Component {
   render() {
     return (
-      <div>
-      App
-      </div>
+      <TodoListTemplate form={<Form/>}>
+        <TodoItemList />
+      </TodoListTemplate>
     );
   }
 }
+
 export default App;
